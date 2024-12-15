82b63e206ae5a5992e8c2b9f60326009dc8a85e4
Peerapat Setsuk <peerapatsetsuk15@gmail.com>
2024-12-16 02:32:10 +0700

    Fix next Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 'use client'
 import Image from "next/image";
 import axios from "axios";
-import { useState , useRef } from "react";
-import { useRouter } from 'next/router';
+import { useState, useEffect  } from "react";
+import { useRouter } from 'next/navigation';
 
 
 export default function Home() {
   const router = useRouter();
+
+
   const goToFormPage = () => {
     router.push('/form');
   };
 
   return (
     <div className="flex flex-row w-screen h-screen p-4 cursor-default">
-      <button onClick={goToFormPage}>Go to About Page</button>
+      <div onClick={goToFormPage}>Go to About Page</div>
     </div>
   );
 }
